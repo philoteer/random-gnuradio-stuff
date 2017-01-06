542dc4b3233ed596b1404e449a99f594398e38f5
philoteer <cdac1234@gmail.com>
2017-01-05 18:19:52 -0800

    Add files via upload

---
+++ blocks/fftshift/FFTshift.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+from gnuradio import gr
+from gnuradio import blocks
+from gnuradio import digital
+import time
+import numpy
+
+#applies fftshift to a vecter.
+#
+class FFTshift(gr.basic_block):
+	#constructor
+	def __init__(self,size,drop_when_overloaded):
+		gr.basic_block.__init__(self, name="FFT_Shift",
+			in_sig=[(numpy.float32,size)],
+			out_sig=[(numpy.float32,size)])
+		self.drop_true = drop_when_overloaded
+
+	#run
+	def general_work(self, input_items, output_items):
+		in0 = input_items[0]
+		out = output_items[0]
+
+		if len(out) >= len(in0):
+			ps_len = len(in0)
+			consume_len = ps_len
+		elif self.drop_true:
+			ps_len = len(out)
+			consume_len = len(in0)
+		else:
+			ps_len = len(out)
+			consume_len = ps_len
+				
+		
+		for cnt in range(0,ps_len):
+			out[cnt] = numpy.fft.fftshift(in0[cnt])
+			
+		#TODO
+		#fftshift
+		self.consume_each(consume_len)
+		return ps_len
